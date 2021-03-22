5a2658ad0681fe114985ad269305c42845075d69
GuYun-D <1946306647@qq.com>
2021-03-22 10:39:04 +0800

    父向子组件传递数据

--- components/Tabs/tabs.js
+++ components/Tabs/tabs.js
@@ -2,32 +2,43 @@
 Component({
   /**
    * 组件的属性列表
+   * 存放的是要从子组件接收的数据
    */
   properties: {
-
+    // 要接收的数据名称
+    // aaa: {
+    //   // type: 要接受的数据类型
+    //   type: String,
+    //   // value: 默认值
+    //   value: ''
+    // }
+    tabs: {
+      type: Array,
+      value: []
+    }
   },
 
   /**
    * 组件的初始数据
    */
   data: {
-    tabs: [{
-      id: 0,
-      name: "首页",
-      isActive: true
-    }, {
-      id: 1,
-      name: "原创",
-      isActive: false
-    }, {
-      id: 2,
-      name: "分类",
-      isActive: false
-    }, {
-      id: 3,
-      name: "关于",
-      isActive: false
-    }, ]
+    // tabs: [{
+    //   id: 0,
+    //   name: "首页",
+    //   isActive: true
+    // }, {
+    //   id: 1,
+    //   name: "原创",
+    //   isActive: false
+    // }, {
+    //   id: 2,
+    //   name: "分类",
+    //   isActive: false
+    // }, {
+    //   id: 3,
+    //   name: "关于",
+    //   isActive: false
+    // }, ]
   },
 
   /**
